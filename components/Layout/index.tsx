909bcd9152e2b102a8f975f0c9887a221fb3d60c
ashish.chodankar <ashish.chodankar@squareyards.co.in>
2023-12-19 10:52:33 +0530

    Layout setup started;implements issue #2

---
+++ components/Layout/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AppHeader from "../molecules/Header/AppHeader";
+
+function Layout() {
+  return (
+    <>
+      <AppHeader />
+    </>
+  );
+}
+
+export default Layout;
