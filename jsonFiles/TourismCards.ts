716ee8aa134a77a468f34054765302953a7bfa6b
ashish.chodankar <ashish.chodankar@squareyards.co.in>
2024-02-19 23:07:45 +0530

    TourismCards response added;implements issue #24

---
+++ jsonFiles/TourismCards.ts
@@ -0,0 +1,10 @@
+export const response = {
+  code: 200,
+  status: "OK",
+  result: [
+    { image: "Image1", touristPlace: "Tikona Fort", grade: "Easy", fee: "₹850" },
+    { image: "Image2", touristPlace: "Rajmachi Fort", grade: "Medium", fee: "₹950" },
+    { image: "Image3", touristPlace: "Sinhagad Fort", grade: "Easy", fee: "₹1050" },
+    { image: "Image4", touristPlace: "Harihar Fort", grade: "Difficult", fee: "₹650" },
+  ],
+};
