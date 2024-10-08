8975c92afe2df8034e82bec9d8b07c71facc69b9
ashish.chodankar <ashish.chodankar@squareyards.co.in>
2024-10-08 19:24:08 +0530

    avtar added in about us page;implements issue #30

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,11 +1,9 @@
 "use client";
 
-import { Box, Container, Typography } from "@mui/material";
+import { Container, Typography } from "@mui/material";
 import Image from "next/image";
 import React from "react";
-import TerrainIcon from "@mui/icons-material/Terrain";
-import PersonIcon from "@mui/icons-material/Person";
-import { Event, HealthAndSafety } from "@mui/icons-material";
+import { staffData, trekWithUs } from "@/jsonFiles/aboutUs";
 
 const About = () => {
   return (
@@ -41,122 +39,53 @@ const About = () => {
         </div>
         <div>
           <h2>Why trek with us?</h2>
-          <div
-            style={{
-              display: "flex",
-              alignItems: "center",
-              gap: "1rem",
-            }}
-          >
-            <TerrainIcon style={{ color: "red" }} />
-            &nbsp;
-            <div>
-              <h3>Varied Terrain and Scenic Routes</h3>
-              <p style={{ fontSize: "1rem" }}>
-                We are offering a wide variety of trekking routes, ranging from
-                easy, scenic paths to challenging alpine terrains. You will
-                experience breathtaking landscapes, forests, waterfalls, and
-                iconic landmarks along the way.
-              </p>
-            </div>
-          </div>
-          <div
-            style={{
-              display: "flex",
-              alignItems: "center",
-              gap: "1rem",
-            }}
-          >
-            <PersonIcon style={{ color: "red" }} />
-            &nbsp;
-            <div>
-              <h3>Experienced Guides</h3>
-              <p
+          {trekWithUs.map(({ id, icon: Icon, title, description }) => {
+            return (
+              <div
                 style={{
                   display: "flex",
                   alignItems: "center",
-                  gap: "1 rem",
+                  gap: "1rem",
                 }}
+                key={id}
               >
-                We are providing highly experienced and certified trekking
-                guides with extensive local knowledge to ensure your trek is
-                safe, informative, and enjoyable. Our guides share insights into
-                the culture, nature, and wildlife of the region.
-              </p>
-            </div>
-          </div>
-          <div
-            style={{
-              display: "flex",
-              alignItems: "center",
-              gap: "1rem",
-            }}
-          >
-            <HealthAndSafety style={{ color: "red" }} />
-            &nbsp;
-            <div>
-              <h3>Safety and First Aid Preparedness</h3>
-              <p
-                style={{
-                  display: "flex",
-                  alignItems: "center",
-                  gap: "1 rem",
-                }}
-              >
-                We are ensuring your safety on every trek with our well-trained
-                staff who are equipped with first aid knowledge and emergency
-                gear. Our team conducts thorough safety briefings and has
-                contingency plans in place for all treks.
-              </p>
-            </div>
-          </div>
-          <div
-            style={{
-              display: "flex",
-              alignItems: "center",
-              gap: "1rem",
-            }}
-          >
-            <Event style={{ color: "red" }} />
-            &nbsp;
-            <div>
-              <h3>Trek for All Seasons</h3>
-              <p
-                style={{
-                  display: "flex",
-                  alignItems: "center",
-                  gap: "1 rem",
-                }}
-              >
-                We are offering year-round trekking experiences, with routes and
-                itineraries tailored to the unique conditions of each season.
-                Whether its summer, winter, or the monsoon, we have treks suited
-                to every time of year.
-              </p>
-            </div>
-          </div>
+                <Icon style={{ color: "red" }} />
+                &nbsp;
+                <div>
+                  <h3>{title}</h3>
+                  <p style={{ fontSize: "1rem" }}>{description}</p>
+                </div>
+              </div>
+            );
+          })}
         </div>
         <div>
           <h2>Staff and Instructors</h2>
-          <div style={{ display: "flex", gap: 30, alignItems: "center" }}>
-            <Image
-              src={"/assets/images/homepage/guy-trek.jpg"}
-              alt="staff1"
-              height={80}
-              width={80}
-              style={{ borderRadius: "50%" }}
-            />
-            <div>
-              <em style={{ fontWeight: "bold" }}>Instructor</em>
-              <h3>Marlo Johnson</h3>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi
-                culpa architecto neque officia, animi consequuntur, voluptas
-                iste earum esse reiciendis laudantium quia. At quasi impedit
-                quibusdam nostrum nesciunt deleniti consequatur?
-              </p>
-            </div>
-          </div>
+          {staffData.map(({ id, src, alt, name, designation, description }) => {
+            return (
+              <div
+                style={{ display: "flex", gap: 30, alignItems: "center" }}
+                key={id}
+              >
+                <Image
+                  src={src}
+                  alt={alt}
+                  height={80}
+                  width={80}
+                  style={{ borderRadius: "50%" }}
+                />
+                <div
+                  style={{ display: "flex", flexDirection: "column", gap: "1" }}
+                >
+                  <em style={{ fontSize: "1.2rem", fontWeight: "bold" }}>
+                    {designation}
+                  </em>
+                  <h3>{name}</h3>
+                  <p>{description}</p>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </Container>
     </>
--- jsonFiles/aboutUs.ts
+++ jsonFiles/aboutUs.ts
@@ -1,30 +1,93 @@
-export const trekWithUs = [
+import {
+  Terrain,
+  Person,
+  HealthAndSafety,
+  SvgIconComponent,
+  Event,
+} from "@mui/icons-material";
+
+type trekWithUs = {
+  id: number;
+  icon: SvgIconComponent;
+  title: string;
+  description: string;
+};
+
+export const trekWithUs: trekWithUs[] = [
   {
     id: 1,
-    icon: "TerrainIcon",
+    icon: Terrain,
     title: "Varied Terrain and Scenic Routes",
     description:
       "We are offering a wide variety of trekking routes, ranging from easy, scenic paths to challenging alpine terrains. You will experience breathtaking landscapes, forests, waterfalls, and iconic landmarks along the way.",
   },
   {
     id: 2,
-    icon: "PersonIcon",
+    icon: Person,
     title: "Experienced Guides",
     description:
       "We are providing highly experienced and certified trekking guides with extensive local knowledge to ensure your trek is safe, informative, and enjoyable. Our guides share insights into the culture, nature, and wildlife of the region.",
   },
   {
     id: 3,
-    icon: "HealthAndSafety",
+    icon: HealthAndSafety,
     title: "Safety and First Aid Preparedness",
     description:
       "We are ensuring your safety on every trek with our well-trained staff who are equipped with first aid knowledge and emergency gear. Our team conducts thorough safety briefings and has contingency plans in place for all treks.",
   },
   {
     id: 4,
-    icon: "Event",
+    icon: Event,
     title: "Trek for All Seasons",
     description:
       "We are offering year-round trekking experiences, with routes and itineraries tailored to the unique conditions of each season. Whether its summer, winter, or the monsoon, we have treks suited to every time of year.",
+  },
+];
+
+export const staffData = [
+  {
+    id: 1,
+    src: "/assets/images/avtar/baldBoy.jpg",
+    alt: "staff1",
+    name: "Yash Chopra",
+    designation: "Instructor",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi culpa architecto neque officia, animi consequuntur, voluptas iste earum esse reiciendis laudantium quia. At quasi impedit quibusdam nostrum nesciunt deleniti consequatur?",
+  },
+  {
+    id: 2,
+    src: "/assets/images/avtar/guy.jpg",
+    alt: "staff2",
+    name: "Ashish Chaudhary",
+    designation: "Instructor",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi culpa architecto neque officia, animi consequuntur, voluptas iste earum esse reiciendis laudantium quia. At quasi impedit quibusdam nostrum nesciunt deleniti consequatur?",
+  },
+  {
+    id: 3,
+    src: "/assets/images/avtar/lensGirl.jpg",
+    alt: "staff3",
+    name: "Sujay Pawar",
+    designation: "Instructor",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi culpa architecto neque officia, animi consequuntur, voluptas iste earum esse reiciendis laudantium quia. At quasi impedit quibusdam nostrum nesciunt deleniti consequatur?",
+  },
+  {
+    id: 4,
+    src: "/assets/images/avtar/man.jpg",
+    alt: "staff4",
+    name: "Amit Rana",
+    designation: "Instructor",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi culpa architecto neque officia, animi consequuntur, voluptas iste earum esse reiciendis laudantium quia. At quasi impedit quibusdam nostrum nesciunt deleniti consequatur?",
+  },
+  {
+    id: 5,
+    src: "/assets/images/avtar/modernGirl.jpg",
+    alt: "staff5",
+    name: "Rahul Patil",
+    designation: "Instructor",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi culpa architecto neque officia, animi consequuntur, voluptas iste earum esse reiciendis laudantium quia. At quasi impedit quibusdam nostrum nesciunt deleniti consequatur?",
   },
 ];
