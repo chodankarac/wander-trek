d7c6cc1d72fb383a375b599fe630862d5fee0ac3
ashish.chodankar <ashish.chodankar@squareyards.co.in>
2024-11-19 22:28:49 +0530

    type error removed from about us page;implements issue #30

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Container, Typography } from "@mui/material";
+import { Box, Container, Grid, Typography } from "@mui/material";
 import Image from "next/image";
 import React from "react";
 import { staffData, trekWithUs } from "@/jsonFiles/aboutUs";
@@ -11,10 +11,18 @@ const About = () => {
       <Container
         maxWidth="md"
         sx={{
-          mt: 4,
+          display: "flex",
+          gap: 4,
+          flexDirection: "column",
         }}
       >
-        <Typography variant="h4" component="h1" gutterBottom align="center">
+        <Typography
+          variant="h4"
+          component="h1"
+          gutterBottom
+          align="center"
+          sx={{ marginTop: 2 }}
+        >
           ABOUT US
         </Typography>
         <Image
@@ -24,9 +32,11 @@ const About = () => {
           width={300}
           style={{ width: "100%", height: "14 rem" }}
         ></Image>
-        <div>
-          <h2>About us</h2>
-          <p style={{ fontSize: "1rem" }}>
+        <Box>
+          <Typography variant="h5" sx={{ fontWeight: "bold" }}>
+            About us
+          </Typography>
+          <Typography style={{ fontSize: "1rem" }}>
             At Trek blazers, we’re dedicated to crafting unforgettable trekking
             experiences for adventurers of all levels. Founded by passionate
             trekkers, we specialize in guiding you through stunning landscapes
@@ -35,35 +45,35 @@ const About = () => {
             <br />
             Whether you’re a beginner or a seasoned hiker, we promise a journey
             that’s memorable, meaningful, and full of breathtaking moments.
-          </p>
-        </div>
-        <div>
-          <h2>Why trek with us?</h2>
-          {trekWithUs.map(({ id, icon: Icon, title, description }) => {
-            return (
-              <div
-                style={{
-                  display: "flex",
-                  alignItems: "center",
-                  gap: "1rem",
-                }}
-                key={id}
-              >
-                <Icon style={{ color: "red" }} />
-                &nbsp;
-                <div>
-                  <h3>{title}</h3>
-                  <p style={{ fontSize: "1rem" }}>{description}</p>
-                </div>
-              </div>
-            );
-          })}
-        </div>
-        <div>
-          <h2>Staff and Instructors</h2>
+          </Typography>
+        </Box>
+        <Box>
+          <Typography variant="h5" sx={{ fontWeight: "bold" }}>
+            Why trek with us?
+          </Typography>
+          <Grid container spacing={3} sx={{ marginTop: 1 }}>
+            {trekWithUs.map(({ id, icon: Icon, title, description }) => {
+              return (
+                <Box key={id} sx={{ display: "flex", gap: 2 }}>
+                  <Icon style={{ color: "red" }} />
+                  <Box
+                    sx={{ display: "flex", flexDirection: "column", gap: 1 }}
+                  >
+                    <Typography variant="h6">{title}</Typography>
+                    <Typography component={"p"} style={{ fontSize: "1rem" }}>
+                      {description}
+                    </Typography>
+                  </Box>
+                </Box>
+              );
+            })}
+          </Grid>
+        </Box>
+        <Box>
+          <Typography variant="h5">Staff and Instructors</Typography>
           {staffData.map(({ id, src, alt, name, designation, description }) => {
             return (
-              <div
+              <Box
                 style={{ display: "flex", gap: 30, alignItems: "center" }}
                 key={id}
               >
@@ -74,7 +84,7 @@ const About = () => {
                   width={80}
                   style={{ borderRadius: "50%" }}
                 />
-                <div
+                <Box
                   style={{ display: "flex", flexDirection: "column", gap: "1" }}
                 >
                   <em style={{ fontSize: "1.2rem", fontWeight: "bold" }}>
@@ -82,11 +92,11 @@ const About = () => {
                   </em>
                   <h3>{name}</h3>
                   <p>{description}</p>
-                </div>
-              </div>
+                </Box>
+              </Box>
             );
           })}
-        </div>
+        </Box>
       </Container>
     </>
   );
